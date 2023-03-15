07d25739fd55c5aa15cd66a7e449f81915838b80
fernandojr999 <fpjpro@gmail.com>
2023-03-15 16:10:34 -0300

    setup inicial do projeto
    Projeto criado com o Spring Initializr.
    
    Linguagem: Java
    Versão: JDK 17
    Spring Boot: 3.0.4

---
+++ src/main/java/br/com/fernando/votingmanager/VotingManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernando.votingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingManagerApplication.class, args);
+	}
+
+}
